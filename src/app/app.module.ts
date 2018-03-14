a37596d3a03e33170a0bcff2e648902c783762dc
Nicki Kim Feldt <nick135f@edu.easj.dk>
2018-03-14 10:46:06 +0100

    Pushing first angular app to hub

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { MyImageComponent } from './my-image/my-image.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyImageComponent
   ],
   imports: [
     BrowserModule
